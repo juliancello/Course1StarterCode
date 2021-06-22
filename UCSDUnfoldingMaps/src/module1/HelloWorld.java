a683643188e08df3ffe055d2fff6c2efbcf3507d
Julian Kosanovic <jkosanovic@natera.com>
2021-06-22 11:58:30 -0700

    Completed module 1

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -1,11 +1,13 @@
 package module1;
 
+//import de.fhpotsdam.unfolding.mapdisplay.OpenGLMapDisplay;
 import processing.core.PApplet;
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.providers.AbstractMapProvider;
+import de.fhpotsdam.unfolding.providers.Microsoft;
 import de.fhpotsdam.unfolding.providers.Google;
-import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
+//import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
 /** HelloWorld
@@ -23,11 +25,11 @@ public class HelloWorld extends PApplet
 	// You can ignore this.  It's to keep eclipse from reporting a warning
 	private static final long serialVersionUID = 1L;
 
-	/** This is where to find the local tiles, for working without an Internet connection */
-	public static String mbTilesString = "blankLight-1-3.mbtiles";
+	// This is where to find the local tiles, for working without an Internet connection
+	// public static String mbTilesString = "blankLight-1-3.mbtiles";
 	
 	// IF YOU ARE WORKING OFFLINE: Change the value of this variable to true
-	private static final boolean offline = false;
+//	private static final boolean offline = false;
 	
 	/** The map we use to display our home town: La Jolla, CA */
 	UnfoldingMap map1;
@@ -36,7 +38,7 @@ public class HelloWorld extends PApplet
 	UnfoldingMap map2;
 
 	public void setup() {
-		size(800, 600, OPENGL);  // Set up the Applet window to be 800x600
+		size(800, 600);  // Set up the Applet window to be 800x600
 		                      // The OPENGL argument indicates to use the 
 		                      // Processing library's 2D drawing
 		                      // You'll learn more about processing in Module 3
@@ -46,17 +48,18 @@ public class HelloWorld extends PApplet
 		this.background(200, 200, 200);
 		
 		// Select a map provider
-		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
+//		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
+		AbstractMapProvider provider = new Microsoft.HybridProvider();
 		// Set a zoom level
 		int zoomLevel = 10;
 		
-		if (offline) {
-			// If you are working offline, you need to use this provider 
-			// to work with the maps that are local on your computer.  
-			provider = new MBTilesMapProvider(mbTilesString);
-			// 3 is the maximum zoom level for working offline
-			zoomLevel = 3;
-		}
+//		if (offline) {
+//			// If you are working offline, you need to use this provider
+//			// to work with the maps that are local on your computer.
+//			provider = new MBTilesMapProvider(mbTilesString);
+//			// 3 is the maximum zoom level for working offline
+//			zoomLevel = 3;
+//		}
 		
 		// Create a new UnfoldingMap to be displayed in this window.  
 		// The 2nd-5th arguments give the map's x, y, width and height
@@ -65,7 +68,7 @@ public class HelloWorld extends PApplet
 		// The 6th argument specifies the map provider.  
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
-		map1 = new UnfoldingMap(this, 50, 50, 0, 250, provider);
+		map1 = new UnfoldingMap(this, 150, 0, 100, 300, provider);
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
 	    map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
@@ -74,10 +77,12 @@ public class HelloWorld extends PApplet
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
-		map2 = new UnfoldingMap(this, 50, 50, 0, 50, provider);
+		map2 = new UnfoldingMap(this, 0, 0, 100, 300, provider);
 		// 45.5428679,-122.7944128
 		map2.zoomAndPanTo(zoomLevel, new Location(45.5f, -122.8f)); // change these coordinates
 		MapUtils.createDefaultEventDispatcher(this, map1, map2);
+//		MapUtils.createDefaultEventDispatcher(this, map1);
+//		MapUtils.createDefaultEventDispatcher(this, map2);
 	}
 
 	/** Draw the Applet window.  */
